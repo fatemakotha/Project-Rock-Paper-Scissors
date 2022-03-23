bbc327b8f9f0f44017c02d9e9d167c4b92c66835
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-23 22:45:39 +0600

    Update main.py
    Fixed the index error that was occurring due to the previous code

--- main.py
+++ main.py
@@ -29,21 +29,26 @@ scissors = '''
 playing_images = [rock, paper, scissors]#rock=0th index, paper=1st index and scissors=2nd index
 
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-print(playing_images[user_choice])
-
-computer_choice = random.randint(0, 2)
-print("Computer chose:")
-print(playing_images[computer_choice])#user_choice here is the index 0,1 or 2
-
 if user_choice >= 3 or user_choice < 0: 
-  print("You typed an invalid number, you lose!") 
-elif user_choice == 0 and computer_choice == 2:
-  print("You win!")
-elif computer_choice == 0 and user_choice == 2:
-  print("You lose")
-elif computer_choice > user_choice:
-  print("You lose")
-elif user_choice > computer_choice:
-  print("You win!")
-elif computer_choice == user_choice:
-  print("It's a draw")
+  print("You typed an invalid number!") 
+else:
+    print(playing_images[user_choice])#user_choice here is the index 0,1 or 2
+    
+    computer_choice = random.randint(0, 2)
+    print("Computer chose:")
+    print(playing_images[computer_choice])
+    
+      
+    if user_choice == 0 and computer_choice == 2:
+      print("You win!")
+      
+    elif computer_choice == 0 and user_choice == 2:
+      print("You lose! ")
+    elif computer_choice > user_choice:
+      print("You lose")
+      
+    elif user_choice > computer_choice:
+      print("You win!")
+      
+    elif computer_choice == user_choice:
+      print("It's a draw! Nobody wins!")
